5587f78961746dc9fff9df16540940182c707017
Vithuran Sadagopan <vithuransoccer1@gmail.com>
2018-01-27 23:14:23 -0500

    Gesture Switch cases made

--- cube_main/cube_main.ino
+++ cube_main/cube_main.ino
@@ -1,3 +1,5 @@
+#include <MyoController.h>
+
 
 #include "led.h"
 #include "cube.h"
@@ -28,6 +30,8 @@ cube CUBE = cube();
 led LED = led();
 //button BUTTON = button();
 
+MyoController myo = MyoController(); //myo Controller object
+
 void setup() {
   //Set ports [0, 12] as output ports
   for (int port = 0; port <= 12; port++) {
@@ -37,6 +41,7 @@ void setup() {
   for (int port = 14; port <= 19; port++) {
     pinMode(port, INPUT);
   }
+  myo.initMyo(); //initialize myo
 
   Serial.begin(9600);
   //randomSeed(analogRead(0));
@@ -45,132 +50,156 @@ void setup() {
 }
 ///////////////////////////////////////////////////VOID LOOP///////////////////////
 void loop() {
-CUBE.diagonalcube(100, 200);
 
+  myo.updatePose();
+  CUBE.diagonalcube(100, 200);
 
-/*  
-  //CUBE.faceONtimed(12,100);
-
-  a = BUTTON.Pushed();
-  //Serial.println(a);
-
-  if (started == 1) {
-    goto GAME;
-  } else if (a == 1) {
-    diff = 1500;
-    CUBE.faceONtimed(1, 100);
-    goto GAME;
-  } else if (a == 2) {
-    diff = 1000;
-    CUBE.faceONtimed(2, 100);
-    goto GAME;
-  } else if (a == 12) {
-    diff = 600;
-    CUBE.faceONtimed(3, 100);
-    goto GAME;
-  } else if (a > 13) {
-    diff = 200;
-    CUBE.faceONtimed(4, 100);
-    goto GAME;
+  switch ( myo.getCurrentPose() ) {
+    case rest:
+      //Stuff for rest gesture
+      break;
+    case fist:
+      //Stuff for fist gesture
+      break;
+    case waveIn:
+      //Stuff for waveIn gesture
+      break;
+    case waveOut:
+      //Stuff for waveOut gesture
+      break;
+    case fingersSpread:
+      //Stuff for fingersSpread gesture
+      break;
+    case doubleTap:
+      //Stuff for doubleTap gesture
+      break;
+    default:
+      //default action
+      break;
   }
-  ///////////////////////////GAME///////////////////////////////////////
-GAME:
-  if (a == 4) {
+  /*
+    //CUBE.faceONtimed(12,100);
 
-    started = 1;
-    //newButton = BUTTON.Pushed() - 3;
+    a = BUTTON.Pushed();
+    //Serial.println(a);
 
-    /////////////////CONTROLS THE LEVEL AND STARTS THE GAME/////////////////
-    for (int x = 1; x < 64; x++) {
-      tone(53, 400);
-      //delay(500);
-      CUBE.ONtimed(500);
-      noTone(53);
-      delay(500);
-      tone(53, 500);
-      CUBE.ONtimed(500);
-      noTone(53);
-      delay(500);
-      tone(53, 500);
-      CUBE.ONtimed(500);
-      noTone(53);
+    if (started == 1) {
+      goto GAME;
+    } else if (a == 1) {
+      diff = 1500;
+      CUBE.faceONtimed(1, 100);
+      goto GAME;
+    } else if (a == 2) {
+      diff = 1000;
+      CUBE.faceONtimed(2, 100);
+      goto GAME;
+    } else if (a == 12) {
+      diff = 600;
+      CUBE.faceONtimed(3, 100);
+      goto GAME;
+    } else if (a > 13) {
+      diff = 200;
+      CUBE.faceONtimed(4, 100);
+      goto GAME;
+    }
+    ///////////////////////////GAME///////////////////////////////////////
+    GAME:
+    if (a == 4) {
+
+      started = 1;
+      //newButton = BUTTON.Pushed() - 3;
+
+      /////////////////CONTROLS THE LEVEL AND STARTS THE GAME/////////////////
+      for (int x = 1; x < 64; x++) {
+        tone(53, 400);
+        //delay(500);
+        CUBE.ONtimed(500);
+        noTone(53);
+        delay(500);
+        tone(53, 500);
+        CUBE.ONtimed(500);
+        noTone(53);
+        delay(500);
+        tone(53, 500);
+        CUBE.ONtimed(500);
+        noTone(53);
 
 
-      faceArray[0] = 0;
-      //////////////TURNS ON THE CUBE PACE IN A RANDOM PATTERN//////////////////
-      for (int y = 1; y <= x; y++) {
-        b = random(4);
-        while (faceArray[y - 1] == b ) {
+        faceArray[0] = 0;
+        //////////////TURNS ON THE CUBE PACE IN A RANDOM PATTERN//////////////////
+        for (int y = 1; y <= x; y++) {
           b = random(4);
+          while (faceArray[y - 1] == b ) {
+            b = random(4);
+          }
+          faceArray[y] = b;
+          CUBE.faceONtimed(faceON[faceArray[y]], diff);
         }
-        faceArray[y] = b;
-        CUBE.faceONtimed(faceON[faceArray[y]], diff);
-      }
-      ////////////////////RECORDS YOUR BUTTON INPUT////////////////////////
-      Time = 10000;
-      start = millis();
-      passed = 0;
-      buttonsPushed[0] = -10;
-      c = 1;
-      oldButton = BUTTON.Pushed() - 3;
+        ////////////////////RECORDS YOUR BUTTON INPUT////////////////////////
+        Time = 10000;
+        start = millis();
+        passed = 0;
+        buttonsPushed[0] = -10;
+        c = 1;
+        oldButton = BUTTON.Pushed() - 3;
 
-      while (Time > passed) {
+        while (Time > passed) {
 
-        CUBE.twoCUBEtimed(14, 50);
-        newButton = BUTTON.Pushed() - 3;
-
-        if (BUTTON.GameButton(3) == 1 && c <= x) {
-          buttonsPushed[c] = newButton;
-          CUBE.faceONtimed(5, 200);
-          c++;
-          oldButton = newButton;
+          CUBE.twoCUBEtimed(14, 50);
           newButton = BUTTON.Pushed() - 3;
-          if ( x < c) {
-            break;
-          }
 
-        } else if (BUTTON.GameButton(4) == 1 && c <= x) {
-          buttonsPushed[c] = newButton;
-          CUBE.faceONtimed(8, 200);
-          c++;
-          oldButton = newButton;
-          newButton = BUTTON.Pushed() - 3;
-          if ( x < c) {
-            break;
-          }
+          if (BUTTON.GameButton(3) == 1 && c <= x) {
+            buttonsPushed[c] = newButton;
+            CUBE.faceONtimed(5, 200);
+            c++;
+            oldButton = newButton;
+            newButton = BUTTON.Pushed() - 3;
+            if ( x < c) {
+              break;
+            }
 
-        } else if (BUTTON.GameButton(5) == 1 && c <= x) {
-          buttonsPushed[c] = newButton;
-          CUBE.faceONtimed(12, 200);
-          c++;
-          oldButton = newButton;
-          newButton = BUTTON.Pushed() - 3;
-          if ( x < c) {
-            break;
-          }
+          } else if (BUTTON.GameButton(4) == 1 && c <= x) {
+            buttonsPushed[c] = newButton;
+            CUBE.faceONtimed(8, 200);
+            c++;
+            oldButton = newButton;
+            newButton = BUTTON.Pushed() - 3;
+            if ( x < c) {
+              break;
+            }
 
-        } else if (BUTTON.GameButton(6) == 1 && c <= x) {
-          buttonsPushed[c] = newButton;
-          CUBE.faceONtimed(9, 200);
-          c++;
-          oldButton = newButton;
-          newButton = BUTTON.Pushed() - 3;
-          if ( x < c) {
-            break;
+          } else if (BUTTON.GameButton(5) == 1 && c <= x) {
+            buttonsPushed[c] = newButton;
+            CUBE.faceONtimed(12, 200);
+            c++;
+            oldButton = newButton;
+            newButton = BUTTON.Pushed() - 3;
+            if ( x < c) {
+              break;
+            }
+
+          } else if (BUTTON.GameButton(6) == 1 && c <= x) {
+            buttonsPushed[c] = newButton;
+            CUBE.faceONtimed(9, 200);
+            c++;
+            oldButton = newButton;
+            newButton = BUTTON.Pushed() - 3;
+            if ( x < c) {
+              break;
+            }
           }
+          Final = millis();
+          passed = Final - start;
         }
-        Final = millis();
-        passed = Final - start;
-      }
-      //////////////////CHECKS IF YOUR BUTTON INPUTS ARE CORRECT///////////////////
-      for (int z = 1; z <= x; z++) {
-        /* Serial.println("compare");
-          Serial.println(faceArray[z]);
-          Serial.println(buttonsPushed[z]);*/
-/*
-        if (faceArray[z] == buttonsPushed[z]) {
-          CUBE.faceONtimed(faceON[faceArray[z]], 200);
-          /* Serial.print("WIN");*//*
+        //////////////////CHECKS IF YOUR BUTTON INPUTS ARE CORRECT///////////////////
+        for (int z = 1; z <= x; z++) {
+          /* Serial.println("compare");
+            Serial.println(faceArray[z]);
+            Serial.println(buttonsPushed[z]);*/
+  /*
+          if (faceArray[z] == buttonsPushed[z]) {
+            CUBE.faceONtimed(faceON[faceArray[z]], 200);
+            /* Serial.print("WIN");*//*
         } else {
           scoreNum = x;
           //Serial.print(scoreNum);
@@ -195,7 +224,7 @@ GAME:
       }
     }
   }
-  */
+*/
 }
 
 
